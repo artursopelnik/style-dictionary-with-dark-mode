f9ece5ad992f0df641120b68d9f3e415dda08465
artursopelnik <artur.sopelnik93@gmail.com>
2024-12-27 15:57:05 +0100

    Initial commit

--- src/buildThemes.ts
+++ src/buildThemes.ts
@@ -1,58 +1,55 @@
-import StyleDictionary from "style-dictionary";
+import StyleDictionary from 'style-dictionary'
 import {
-    logBrokenReferenceLevels,
-    logVerbosityLevels,
-    logWarningLevels,
-} from "style-dictionary/enums";
-import { Config } from "style-dictionary/types";
+  logBrokenReferenceLevels,
+  logVerbosityLevels,
+  logWarningLevels,
+} from 'style-dictionary/enums'
+import { Config } from 'style-dictionary/types'
 
-const themes = ["light", "dark"];
+const themes = ['light', 'dark']
 
 const CONFIG: Config = {
-    log: {
-        warnings: logWarningLevels.warn,
-        verbosity: logVerbosityLevels.default,
-        errors: {
-            brokenReferences: logBrokenReferenceLevels.throw,
-        },
+  log: {
+    warnings: logWarningLevels.warn,
+    verbosity: logVerbosityLevels.default,
+    errors: {
+      brokenReferences: logBrokenReferenceLevels.throw,
     },
-};
+  },
+}
 
 const buildThemes = (async () => {
-    await Promise.all(
-        themes.map((theme) => {
-            const isLight = theme === "light";
+  await Promise.all(
+    themes.map((theme) => {
+      const isLight = theme === 'light'
 
-            // this is saying find any files in the tokens folder
-            // that does not have dark or light
-            const src = isLight ? `!(*.${themes.join(`|*.`)})` : `*.${theme}`
+      // this is saying find any files in the tokens folder
+      // that does not have dark or light
+      const src = isLight ? `!(*.${themes.join(`|*.`)})` : `*.${theme}`
 
-            const sd = new StyleDictionary({
-                ...CONFIG,
-                source: [
-                    `tokens/**/${src}.json`,
-                    `tokens/**/${src}.json5`,
-                ],
-                platforms: {
-                    css: {
-                        transformGroup: "css",
-                        buildPath: "build/css/",
-                        files: [
-                            {
-                                destination: `variables.${theme}.css`,
-                                format: `css/variables`,
-                                options: {
-                                    selector: `:root, :host, .theme-${theme}`,
-                                    outputReferences: true,
-                                },
-                            },
-                        ],
-                    },
+      const sd = new StyleDictionary({
+        ...CONFIG,
+        source: [`tokens/**/${src}.json`, `tokens/**/${src}.json5`],
+        platforms: {
+          css: {
+            transformGroup: 'css',
+            buildPath: 'build/css/',
+            files: [
+              {
+                destination: `variables.${theme}.css`,
+                format: `css/variables`,
+                options: {
+                  selector: `:root, :host, .theme-${theme}`,
+                  outputReferences: true,
                 },
-            });
-            return sd.buildAllPlatforms();
-        }),
-    );
-})();
+              },
+            ],
+          },
+        },
+      })
+      return sd.buildAllPlatforms()
+    }),
+  )
+})()
 
 export default buildThemes
